f93e083535e2301bce2c05a8976f2d2a96e7fd89
Brice LESNE <brice@pc9.home>
2020-09-11 16:51:47 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# cra-template-reacteur
+
+A clean start Create React App template ! ;)
+
+```bash
+npx create-react-app my-app --template cra-template-reacteur
+```
---
+++ template/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+    </div>
+  );
+}
+
+export default App;
